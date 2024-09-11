1f3fb47e85a0160b756d90a2d369bc6e8d5fbc8e
Abhishek Jain <abhishek.jain11@nagarro.com>
2024-09-11 13:51:03 +0530

    Initial Commit for Sample Web App

---
+++ webapp/src/main/java/com/sample/Simple.java
@@ -0,0 +1,24 @@
+package com.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Simple extends HttpServlet {
+
+   private static final long serialVersionUID = 1L; 
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response)  
+      throws ServletException, IOException { 
+	   System.out.println("Hello World!");
+      response.setContentType("text/html");   
+      PrintWriter out = response.getWriter();   
+      out.print("<html><body>");   
+      out.print("<h3>Hello Servlet</h3>");   
+      out.print("</body></html>");         
+   }   
+}
