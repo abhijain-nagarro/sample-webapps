5601fab11ef6f2d6d9bc72daf629ee22cca96cad
abhijain-nagarro <abhishek.jain11@nagarro.com>
2024-09-11 16:33:10 +0530

    Update Simple.java

--- webapp/src/main/java/com/sample/Simple.java
+++ webapp/src/main/java/com/sample/Simple.java
@@ -18,7 +18,7 @@ public class Simple extends HttpServlet {
       response.setContentType("text/html");   
       PrintWriter out = response.getWriter();   
       out.print("<html><body>");   
-      out.print("<h3>Hello Servlet</h3>");   
+      out.print("<h3>Hello Servlet1</h3>");   
       out.print("</body></html>");         
    }   
 }
